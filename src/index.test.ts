8b6b0c321a72a056f97a4ad22f499690b7f80118
l1b3r_- <liber@ricardojrm.com>
2021-12-03 03:37:14 +0000

    feat: color-converter

---
+++ src/index.test.ts
@@ -0,0 +1,18 @@
+import { hexToRgb, rgbToHex } from './index';
+
+describe('converts colors', () => {
+  const initialHex = '#30a14e';
+  const initialRgb = {
+    r: 48,
+    g: 161,
+    b: 78,
+  };
+
+  it('should convert "#30a14e" to rgb(48 161 78)', () => {
+    expect(hexToRgb(initialHex)).toEqual(initialRgb);
+  });
+
+  it('should convert rgb(48 161 78) to "#30a14e"', () => {
+    expect(rgbToHex(initialRgb.r, initialRgb.g, initialRgb.b)).toBe(initialHex);
+  });
+});
